db00ca6b784a8f2c32c2f5c99b95b415d6b93ce0
Dayvon Allen <dayvonallen@Dayvons-MBP.attlocal.net>
2021-03-06 18:59:34 -0600

    finished backend

--- contracts/TestTokenSale.sol
+++ contracts/TestTokenSale.sol
@@ -3,20 +3,23 @@ pragma solidity >=0.4.22 <0.9.0;
 
 import "./TestToken.sol";
 
+// ICO, how it works
+
+// Provision tokens to token sale contract
+// Set a token price in wei
+// assign an admin
+// buy tokens
+// end sale
+
 contract TestTokenSale {
-    
-    address admin;
+    address payable admin;
     TestToken public tokenContract;
     uint256 public tokenPrice;
     uint256 public tokensSold;
 
-    event Sell(
-        address _buyer,
-        uint256 _amount
-    );
+    event Sell(address _buyer, uint256 _amount);
 
     constructor(TestToken _tokenContract, uint256 _tokenPrice) public {
-        
         // assign an admin
         admin = msg.sender;
 
@@ -27,11 +30,50 @@ contract TestTokenSale {
         tokenPrice = _tokenPrice;
     }
 
+    // multiply helper
+    // pure means it's not creating any transactions or reading and writing to the blockchain
+    function multiply(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
     // Buy Tokens
     function buyToken(uint256 _numberOfTokens) public payable {
+        //require that value is equal to tokens
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
 
+        // require contract has enough tokens
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+
+        //require successful transfer
+        //when you use require it will revert if it doesn't happen
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
+
+        // sell token
         tokensSold += _numberOfTokens;
 
         emit Sell(msg.sender, _numberOfTokens);
+    }
+
+    // ending token sale
+    function endSale() public {
+        // require only an admin can do it
+        require(msg.sender == admin);
+
+        // transfer remaining tokens back to admin
+        // transfer allocated tokens back to the
+        // token contract
+        // address(this) is the current contract
+        // converted to an address.
+        require(
+            tokenContract.transfer(
+                admin,
+                tokenContract.balanceOf(address(this))
+            )
+        );
+        // destroy contract
+        // selfdestruct - destroys the current contract, sending its funds to the given address
+        //suicide(alias to self destruct)
+        // when a contract is destroyed all of the state is going to be cleared out(variables)
+        selfdestruct(admin);
     }
 }
--- test/TestTokenSale.js
+++ test/TestTokenSale.js
@@ -1,12 +1,15 @@
 const TestTokenSale = artifacts.require("./TestTokenSale.sol");
+const TestToken = artifacts.require("./TestToken.sol");
 
 // accounts are from ganache
 contract("TestTokenSale", (accounts) => {
   let tokenSaleInstance;
+  let tokenInstance;
+  let admin = accounts[0];
   let tokenPrice = 10000000000000000; // wei
   let buyer = accounts[1];
+  let tokenAvailable = 750000;
   let numberOfTokens = 10;
-
 
   it("Initializes the contract with the correct values", () => {
     return TestTokenSale.deployed()
@@ -28,10 +31,28 @@ contract("TestTokenSale", (accounts) => {
   });
 
   it("facilitates token buying", () => {
-      return TestTokenSale.deployed().then((instance) => {
+    return TestToken.deployed()
+      .then((instance) => {
+        tokenInstance = instance;
+        return TestTokenSale.deployed();
+      })
+      .then((instance) => {
         tokenSaleInstance = instance;
-        return tokenSaleInstance.buyToken(numberOfTokens, {from: buyer, value: numberOfTokens * tokenPrice})
-      }).then((receipt) => {
+
+        // provision 75% of totla supply to the sale
+        return tokenInstance.transfer(
+          tokenSaleInstance.address,
+          tokenAvailable,
+          { from: admin }
+        );
+      })
+      .then((receipt) => {
+        return tokenSaleInstance.buyToken(numberOfTokens, {
+          from: buyer,
+          value: numberOfTokens * tokenPrice,
+        });
+      })
+      .then((receipt) => {
         assert.equal(receipt.logs.length, 1, "triggers one event");
         assert.equal(
           receipt.logs[0].event,
@@ -43,11 +64,91 @@ contract("TestTokenSale", (accounts) => {
           buyer,
           "logs the account that purchased the tokens"
         );
-        assert.equal(receipt.logs[0].args._amount, numberOfTokens, "Logs the number of tokens purchased");
-        
+        assert.equal(
+          receipt.logs[0].args._amount,
+          numberOfTokens,
+          "Logs the number of tokens purchased"
+        );
+
         return tokenSaleInstance.tokensSold();
-      }).then((amount) => {
-          assert.equal(amount.toNumber(), numberOfTokens, "increments the number of tokens sold");
       })
-  })
+      .then((amount) => {
+        assert.equal(
+          amount.toNumber(),
+          numberOfTokens,
+          "increments the number of tokens sold"
+        );
+
+        return tokenInstance.balanceOf(buyer);
+      })
+      .then((balance) => {
+        assert.equal(
+          balance.toNumber(),
+          numberOfTokens,
+          "Should increase buyer tokens."
+        );
+        return tokenInstance.balanceOf(tokenSaleInstance.address);
+      })
+      .then((balance) => {
+        assert.equal(
+          balance.toNumber(),
+          tokenAvailable - numberOfTokens,
+          "Should reduce contract balance."
+        );
+        return tokenSaleInstance.buyToken(numberOfTokens, {
+          from: buyer,
+          value: 1,
+        });
+      })
+      .then(assert.fail)
+      .catch((error) => {
+        assert(
+          error.message.indexOf("revert") >= 0,
+          "Should fail when underpaying for token"
+        );
+        return tokenSaleInstance.buyToken(numberOfTokens, {
+          from: buyer,
+          value: 800000,
+        });
+      })
+      .then(assert.fail)
+      .catch((error) => {
+        assert(
+          error.message.indexOf("revert") >= 0,
+          "Should fail when purchasing more tokens than available"
+        );
+      });
+  });
+
+  it("Ends token sale", () => {
+    return TestToken.deployed()
+      .then((instance) => {
+        tokenInstance = instance;
+        return TestTokenSale.deployed();
+      })
+      .then((instance) => {
+        tokenSaleInstance = instance;
+
+        // try to end account from non admin account
+        return tokenSaleInstance.endSale({ from: buyer });
+      })
+      .then(assert.fail)
+      .catch((error) => {
+        assert(
+          error.message.indexOf("revert") >= 0,
+          "Should fail when trying to end sale with someone other than the admin"
+        );
+        return tokenSaleInstance.endSale({ from: admin });
+      })
+      .then((receipt) => {
+        return tokenInstance.balanceOf(admin);
+      })
+      .then((balance) => {
+        assert.equal(
+          balance.toNumber(),
+          999990,
+          "Should return remaining balancing back to admin when ICO ends"
+        );
+      });
+  });
 });
